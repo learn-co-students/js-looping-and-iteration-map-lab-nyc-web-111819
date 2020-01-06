3919905e7544134997f9220235bf6faf6e601f18
Shakil Abdin <shakil.abdin@gmail.com>
2020-01-06 10:15:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(obj) {
+    return obj.map(function(driver) { 
+        return driver.toLowerCase();
+    });
+}
+
+function nameToAttributes(obj) {
+    return obj.map(function(driver) { 
+        const first = driver.split(" ")[0];
+        const last = driver.split(" ")[1];
+        return {firstName: first, lastName: last};
+
+    });
+}
+
+function attributesToPhrase(obj) {
+    return obj.map(function(driver) {
+        return `${driver.name} is from ${driver.hometown}`
+    });
+}
